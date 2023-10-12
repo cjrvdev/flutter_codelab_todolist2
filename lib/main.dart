232cf3666cb3111fccae248f8e3289a96e5ea8af
Carlos Rodriguez <cj.rodriguez.vega@outlook.com>
2023-10-12 23:16:12 +0200

    Upgrade with taskprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(ProviderScope(child: MainApp()));
+
+final tasksProvider = StateNotifierProvider<TaskNotifier, List<Task>>((ref) {
+  return TaskNotifier(tasks: [
+    Task(id: 1, label: 'Salir'),
+    Task(id: 2, label: 'Beber'),
+    Task(id: 3, label: 'El mismo rollo de siempre'),
+  ]);
+});
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -23,64 +30,106 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('My Todolist app')),
       body: Column(children: [
-        Text('Estas son tus tareas pendientes'),
+        SizedBox(
+          height: 10,
+        ),
         Progress(),
+        SizedBox(
+          height: 10,
+        ),
+        TaskList()
       ]),
     );
   }
 }
 
-class Progress extends StatelessWidget {
+class Progress extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    var tasks = ref.watch(tasksProvider);
+
+    var numCompletedTasks = tasks.where((task) {
+      return task.completed == true;
+    }).length;
+
     return Column(
       children: [
         Text('Progreso de las tareas'),
-        LinearProgressIndicator(
-          value: 0,
+        SizedBox(
+          height: 5,
         ),
-        TaskList()
+        LinearProgressIndicator(
+          value: numCompletedTasks / tasks.length,
+        ),
       ],
     );
   }
 }
 
-class TaskList extends StatelessWidget {
+class TaskList extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    var tasks = ref.watch(tasksProvider);
+
     return Column(
-      children: [
-        TaskItem(label: 'Comer'),
-        TaskItem(label: 'Beber'),
-        TaskItem(label: 'New World'),
-        TaskItem(label: 'La droga'),
-        TaskItem(label: 'Meterme con Judith')
-      ],
+      children: tasks
+          .map(
+            (task) => TaskItem(task: task),
+          )
+          .toList(),
     );
   }
 }
 
-class TaskItem extends StatefulWidget {
-  final String label;
+class TaskItem extends ConsumerWidget {
+  final Task task;
 
-  TaskItem({super.key, required this.label});
-
-  @override
-  State<TaskItem> createState() => _TaskItemState();
-}
-
-class _TaskItemState extends State<TaskItem> {
-  bool? _selected = false;
+  TaskItem({super.key, required this.task});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Row(
       children: [
         Checkbox(
-            value: _selected,
-            onChanged: (newValue) => setState(() => _selected = newValue)),
-        Text(widget.label)
+            value: task.completed,
+            onChanged: (newValue) =>
+                ref.read(tasksProvider.notifier).toggle(task.id)),
+        Text(task.label)
       ],
     );
+  }
+}
+
+@immutable
+class Task {
+  final int id;
+  final String label;
+  final bool completed;
+
+  Task({required this.id, required this.label, this.completed = false});
+
+  Task copyWith({int? id, String? label, bool? completed}) {
+    return Task(
+        id: id ?? this.id,
+        label: label ?? this.label,
+        completed: completed ?? this.completed);
+  }
+}
+
+class TaskNotifier extends StateNotifier<List<Task>> {
+  TaskNotifier({tasks}) : super(tasks);
+
+  void add(Task task) {
+    state = [...state, task];
+  }
+
+  void toggle(int taskId) {
+    state = [
+      for (final item in state)
+        if (taskId == item.id)
+          item.copyWith(completed: !item.completed)
+        else
+          item
+    ];
   }
 }
